4a5999fd3c95fa591728d43f619d4d7af7752632
Ayman Alaswad <aymandeveloper0git@gmail.com>
2023-11-15 12:39:22 +0300

    Add some colors

--- lib/shared/styles/colors.dart
+++ lib/shared/styles/colors.dart
@@ -4,20 +4,21 @@ class AppColors {
   // static final Color darkColor = Colors.indigo;
   static final Color darkColor = Color(0xff353A4F);
   static final Color darkLightColor = Color(0xff808B96);
+  static final Color primaryDark = Color(0xff4B527E);
 
   static const MaterialColor primaryColor = Colors.indigo;
   static final Color primaryColorS200 = Colors.indigo.shade200;
   static final Color primaryColorS300 = Colors.indigo.shade300;
-  static final Color primaryColorS700 = Colors.indigo.shade700;
 
   static const Color grey = Colors.grey;
   static final Color greyS50 = Colors.grey.shade50;
   static final Color greyS100 = Colors.grey.shade100;
   static final Color greyS200 = Colors.grey.shade200;
+  static final Color greyS300 = Colors.grey.shade300;
   static final Color greyS400 = Colors.grey.shade400;
-  static final Color greyS500 = Colors.grey.shade500;
   static final Color greyS600 = Colors.grey.shade600;
 
+  static const Color white24 = Colors.white24;
   static const Color white = Colors.white;
   static const Color black = Colors.black;
   static const Color red = Colors.red;
