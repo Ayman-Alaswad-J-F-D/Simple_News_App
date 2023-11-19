2e392e9d78fe15c71420a43686b573fcd8f531cb
Ayman Alaswad <aymandeveloper0git@gmail.com>
2023-11-19 21:21:49 +0300

    Convert the function to stateless widget and add some properties

--- lib/shared/components/build_desktop.dart
+++ lib/shared/components/build_desktop.dart
@@ -1,25 +1,173 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-import '../cubit/cubit.dart';
-import 'desktop_items.dart';
+import '../../generated/l10n.dart';
+import '../../logic/cubit/cubit.dart';
+import '../../logic/models/news_model.dart';
+import '../styles/colors.dart';
+import 'build_articales.dart';
+import 'custom_cache_network_image.dart';
 
-class BuilderDesktop extends StatelessWidget {
-  const BuilderDesktop({
-    Key? key,
-    required this.cubit,
-    required this.list,
-  }) : super(key: key);
+class DesktopBuilder extends StatelessWidget {
+  const DesktopBuilder({Key? key, required this.atricles}) : super(key: key);
 
-  final BreakingNewsAppCubit cubit;
-  final List list;
+  final List<ArticlesModel> atricles;
 
   @override
   Widget build(BuildContext context) {
-    return Builder(
-      builder: (BuildContext context) {
-        cubit.isDesktop ? DesktopItems(list: list) : cubit.setDesktop(true);
-        return DesktopItems(list: list);
-      },
+    var cubit = BreakingNewsAppCubit.get(context);
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: ArticlesBuilder(articles: atricles),
+        ),
+        if (atricles.length > 0)
+          Expanded(
+            child: Container(
+              color: Theme.of(context).cardColor,
+              height: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomImage(
+                      imageUrl: atricles[cubit.selectedItem].urlToImage ?? '',
+                    ),
+                    const SizedBox(height: 20),
+                    TitleWidget(
+                      title: atricles[cubit.selectedItem].title ?? '',
+                    ),
+                    const SizedBox(height: 10),
+                    DescriptionWidget(
+                      description:
+                          atricles[cubit.selectedItem].description ?? '',
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        PublishedAtWidget(
+                          publishAt:
+                              atricles[cubit.selectedItem].publishedAt ?? '',
+                        ),
+                        LinkWidget(url: atricles[cubit.selectedItem].url ?? '')
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+}
+
+class CustomImage extends StatelessWidget {
+  const CustomImage({
+    Key? key,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: 400,
+        width: double.infinity,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15.0),
+          child: CustomCachedNetworkImage(
+            desktop: true,
+            imageUrl: imageUrl,
+            fitImage: BoxFit.fill,
+            icon: Icons.error,
+            sizeIcon: 150,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TitleWidget extends StatelessWidget {
+  const TitleWidget({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      maxLines: 3,
+      overflow: TextOverflow.ellipsis,
+      style: Theme.of(context).textTheme.bodyText1?.copyWith(fontSize: 24),
+    );
+  }
+}
+
+class DescriptionWidget extends StatelessWidget {
+  const DescriptionWidget({
+    Key? key,
+    required this.description,
+  }) : super(key: key);
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      description.isEmpty ? S.of(context).NoFoundDescription : description,
+      maxLines: 5,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(fontSize: 17, color: AppColors.greyS600),
+    );
+  }
+}
+
+class PublishedAtWidget extends StatelessWidget {
+  const PublishedAtWidget({
+    Key? key,
+    required this.publishAt,
+  }) : super(key: key);
+
+  final String publishAt;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      DateFormat.yMMMMEEEEd().format(DateTime.parse(publishAt)),
+      style: Theme.of(context).textTheme.bodyText2,
+    );
+  }
+}
+
+class LinkWidget extends StatelessWidget {
+  const LinkWidget({
+    Key? key,
+    required this.url,
+  }) : super(key: key);
+
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text("News Link", style: Theme.of(context).textTheme.caption),
+        IconButton(
+          icon: Icon(Icons.link, color: Theme.of(context).iconTheme.color),
+          onPressed: () => launchUrl(Uri.parse(url)),
+        )
+      ],
     );
   }
 }
--- lib/shared/components/custom_text_form_field.dart
+++ lib/shared/components/custom_text_form_field.dart
@@ -3,56 +3,87 @@ import 'package:flutter/material.dart';
 import '../styles/colors.dart';
 import '../styles/styles.dart';
 
-Widget CustomTextFormField({
-  required TextEditingController textEditingController,
-  required TextInputType typeInput,
-  required String label,
-  double radius = 15.0,
-  double gapPadding = 5.0,
-  Icon? prefixIcon,
-  IconData? suffixIcon,
-  Function()? suffixPressed,
-  Function()? onSubmit,
-  Function(String)? onChange,
-  Function()? onTap,
-  String? Function(String?)? validate,
-  bool isPassword = false,
-  bool filledneed = false,
-  Color fillColor = AppColors.white,
-  String? hintText,
-  Color? colorHintText,
-  double? fintSizeHintText,
-}) =>
-    TextFormField(
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    Key? key,
+    this.textEditingController,
+    required this.typeInput,
+    required this.label,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.suffixPressed,
+    this.onSubmit,
+    this.onChange,
+    this.onTap,
+    this.validate,
+    this.hintText,
+    this.colorHintText,
+    this.fintSizeHintText,
+    this.radius = 8.0,
+    this.gapPadding = 5.0,
+    this.isPassword = false,
+    this.filledneed = false,
+    this.fillColor = AppColors.white,
+  }) : super(key: key);
+
+  final TextEditingController? textEditingController;
+  final TextInputType typeInput;
+  final String label;
+  final double radius;
+  final double gapPadding;
+  final Icon? prefixIcon;
+  final IconData? suffixIcon;
+  final Function()? suffixPressed;
+  final Function()? onSubmit;
+  final Function(String)? onChange;
+  final Function()? onTap;
+  final String? Function(String?)? validate;
+  final bool isPassword;
+  final bool filledneed;
+  final Color fillColor;
+  final String? hintText;
+  final Color? colorHintText;
+  final double? fintSizeHintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
       onChanged: onChange,
       onTap: onTap,
       validator: validate,
       controller: textEditingController,
       keyboardType: typeInput,
       obscureText: isPassword,
-      // style: const TextStyle(color: Colors.indigo),
+      style: TextStyle(color: Theme.of(context).textTheme.caption?.color),
       decoration: InputDecoration(
         filled: filledneed,
         fillColor: filledneed ? fillColor : AppColors.white,
         labelText: label,
-        labelStyle: const TextStyle(
+        labelStyle: TextStyle(
           fontFamily: AppFonts.primaty,
+          fontSize: 18,
+          color: Theme.of(context).textTheme.bodyMedium?.color,
         ),
         hintText: hintText,
         hintStyle: TextStyle(
           color: colorHintText,
           fontSize: fintSizeHintText,
         ),
-        border: OutlineInputBorder(
-          gapPadding: gapPadding,
-          borderRadius: BorderRadius.all(Radius.circular(radius)),
-        ),
+        // border: OutlineInputBorder(
+        //   gapPadding: gapPadding,
+        //   borderRadius: BorderRadius.all(Radius.circular(radius)),
+        // ),
         prefixIcon: prefixIcon,
         suffixIcon: suffixIcon != null
             ? IconButton(
                 onPressed: suffixPressed,
-                icon: Icon(suffixIcon),
+                icon: Icon(
+                  suffixIcon,
+                  color: Theme.of(context).iconTheme.color,
+                ),
               )
             : null,
       ),
     );
+  }
+}
