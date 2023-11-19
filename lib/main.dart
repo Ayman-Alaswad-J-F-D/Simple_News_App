d945e9c96b5103175eed66665dbb439127bc341a
Ayman Alaswad <aymandeveloper0git@gmail.com>
2023-11-19 11:08:34 +0300

    Add local and localizationsDelegates properties and clean entrypoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,64 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get_storage/get_storage.dart';
-
-import 'app_cubit/app_cubit.dart';
-import 'app_cubit/app_states.dart';
-import 'modules/splash_screen.dart';
-import 'shared/bloc_observer.dart';
-import 'shared/cubit/cubit.dart';
+import 'package:intl/intl.dart';
+import 'package:new_app/modules/splash_screen.dart';
+import 'package:new_app/shared/constants.dart';
+import 'generated/l10n.dart';
+import 'bloc_observer.dart';
+import 'logic/cubit/cubit.dart';
+import 'logic/cubit/states.dart';
 import 'shared/network/local/cache_helper.dart';
 import 'shared/network/remote/dio_helper.dart';
 import 'shared/themes/themes.dart';
 
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-  }
-}
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // set minmum width and height on app
-  // if (Platform.isWindows) {
-  //   await DesktopWindow.setMinWindowSize(Size(500, 700));
-  // }
-  // final url = "https://www.google.com";
-  // Webview(true)
-  //     .setTitle("title")
-  //     .setSize(1280, 800, SizeHint.none)
-  //     .navigate(url)
-  //     .run();
-
+  await DioHelper.init();
   await GetStorage.init();
-  bool? isDark = CacheHelper.getData(key: 'isDark') ?? false;
-  BlocOverrides.runZoned(
-    () {
-      // Use cubits...
-      DioHelper.init();
-      HttpOverrides.global = MyHttpOverrides();
-      runApp(BreakingNewsApp(isDark));
-    },
-    blocObserver: MyBlocObserver(),
-  );
+  final currentLocale = Intl.getCurrentLocale().substring(0, 2);
+  bool isDark = CacheHelper.getData(key: 'isDark') ?? false;
+  String language = CacheHelper.getData(key: 'language') ?? currentLocale;
+
+  Bloc.observer = MyBlocObserver();
+  runApp(BreakingNewsApp(isDark: isDark, language: language));
 }
 
 class BreakingNewsApp extends StatelessWidget {
-  const BreakingNewsApp(this.isDark);
+  const BreakingNewsApp({required this.isDark, required this.language});
 
-  final bool? isDark;
+  final bool isDark;
+  final String language;
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => AppCubit()..changeDarkMode(fromShared: isDark),
-        ),
-        BlocProvider(
-          create: (context) => BreakingNewsAppCubit()
-            ..getGeneralData()
-            ..getSportsData()
-            ..getTechnologyData(),
-        ),
-      ],
-      child: BlocBuilder<AppCubit, AppStates>(
+    return BlocProvider(
+      create: (context) {
+        return BreakingNewsAppCubit()
+          ..getFromCache(languageFromCache: language, modeFromCache: isDark);
+      },
+      child: BlocConsumer<BreakingNewsAppCubit, BreakingNewsAppStates>(
+        listener: (context, state) {
+          if (state is ChangeLanguageAppState) {
+            BreakingNewsAppCubit.get(context).getLanguage();
+          }
+        },
         builder: (context, state) {
+          final cubit = BreakingNewsAppCubit.get(context);
           return MaterialApp(
+            locale: cubit.appLanguage == AR ? Locale(AR) : Locale(EN),
+            localizationsDelegates: const [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
             debugShowCheckedModeBanner: false,
             theme: lightTheme,
             darkTheme: darkTheme,
-            themeMode:
-                AppCubit.get(context).isDark ? ThemeMode.dark : ThemeMode.light,
+            themeMode: cubit.isDark ? ThemeMode.dark : ThemeMode.light,
             home: const SplashScreen(),
           );
         },
