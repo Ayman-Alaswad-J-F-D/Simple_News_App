2b49e4f5bbc3296c5fb1e0a508bad3281c671866
Ayman Alaswad <aymandeveloper0git@gmail.com>
2023-11-19 09:25:49 +0300

    Add some constants variables

---
+++ lib/shared/constants.dart
@@ -0,0 +1,10 @@
+const String API_KEY = '8dd5612c0c8c4ccb9ca226db963bef93';
+
+const String GENERAL = 'general';
+const String SPORTS = 'sports';
+const String TECHNOLOGY = 'technology';
+const String US_COUNTRY = 'us';
+const String EG_COUNTRY = 'eg';
+
+const String AR = 'ar';
+const String EN = 'en';
